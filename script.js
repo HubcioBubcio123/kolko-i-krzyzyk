f8586ff1ca3d5e4a7fe9d66bdd1dfb9401bd105a
HubcioBubcio123 <hubonbubon@gmail.com>
2026-05-16 17:18:19 +0200

    adding game logic, detecting win, finishing tic-tac-toe

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
 const komorki = document.querySelectorAll(".komorka")
 const btnNew = document.querySelector(".btn_new-game")
-
-
-komorki.forEach(function(komorka) {
+let aktualnyGracz = "O"
+let plansza = ["", "", "", "", "", "", "", "", ""]
+let koniecGry = false
+const kombinacje = [
+    [0, 1, 2], 
+    [3, 4, 5],
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6], 
+]
+function sprawdzWygrana() {
+    for(let i = 0; i < kombinacje.length; i++) {
+        const [a, b, c] = kombinacje[i]
+        
+        if(plansza[a] !== "" && plansza[a] === plansza[b] && plansza[a] === plansza[c]) {
+            alert(plansza[a] + " wygrywa!")
+            koniecGry = true 
+            return true
+        }
+    }
+    return false
+}
+komorki.forEach(function(komorka,index) {
     komorka.addEventListener("click", function() {
-       komorka.classList.add("krzyzyk")
-    })
+        if(koniecGry) return
+        if(komorka.classList.contains("krzyzyk") || komorka.querySelector("svg")){
+            return
+        }
+     
+        if(aktualnyGracz === "X"){
+          komorka.classList.add("krzyzyk")
+      }else {
+         const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+         const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle")
+         circle.setAttribute("cx", "50")
+         circle.setAttribute("cy", "50")
+         circle.setAttribute("r", "40")
+         svg.setAttribute("viewBox", "0 0 100 100")
+         svg.appendChild(circle)
+         komorka.appendChild(svg)
+      }
+     plansza[index] = aktualnyGracz
+    sprawdzWygrana()
+
+      if(aktualnyGracz === "X") {
+      aktualnyGracz = "O"
+     } else {
+      aktualnyGracz = "X"
+    }
+    
+    
 })
 
-
+})
 
 btnNew.addEventListener("click", function() {
     komorki.forEach(function(komorka) {
     komorka.classList.remove("krzyzyk")
+    komorka.innerHTML = ""
+    plansza = ["", "", "", "", "", "", "", "", ""]
+    koniecGry = false
 })
   
 })
+
+
+
+
 
 
