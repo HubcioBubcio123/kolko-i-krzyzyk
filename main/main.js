5ef08b726bd4ea7a7174739f4ae5aa1b703c5910
Franeksztylka <franeksztylka99@gmail.com>
2026-05-19 20:57:22 +0200

    background changes

--- main/main.js
+++ main/main.js
@@ -1,8 +1,18 @@
+const bgAnimation = document.getElementById('bgAnimation')
+
+const numberOfColorBoxes = 400
+
 document.querySelectorAll('.karta').forEach(karta => {
-  karta.addEventListener('mouseenter', () => {
-    document.body.classList.add('karta-hover');
-  });
-  karta.addEventListener('mouseleave', () => {
-    document.body.classList.remove('karta-hover');
-  });
-});
+	karta.addEventListener('mouseenter', () => {
+		document.body.classList.add('karta-hover')
+	})
+	karta.addEventListener('mouseleave', () => {
+		document.body.classList.remove('karta-hover')
+	})
+})
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+	const colorBox = document.createElement('div')
+	colorBox.classList.add('colorBox')
+	bgAnimation.append(colorBox)
+}
