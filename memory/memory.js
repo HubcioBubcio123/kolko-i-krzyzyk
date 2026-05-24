1e6cb83694c1b3ce83d67b3ff6441737aaf80d85
HubcioBubcio123 <hubonbubon@gmail.com>
2026-05-25 00:19:41 +0200

    finishing memory game, adding logic and final design touches

--- memory/memory.js
+++ memory/memory.js
@@ -1,5 +1,19 @@
 const canvas = document.getElementById('particles');
 const ctx = canvas.getContext('2d');
+//<-------DO LOGIKI MEMORY----->
+const karty = [
+  "../img/gory-memory.jpg", "../img/gory-memory.jpg",
+   "../img/wintersosny-memory.jpg", "../img/wintersosny-memory.jpg",
+    "../img/iceberg.jpg", "../img/iceberg.jpg",
+     "../img/waterfall.jpg", "../img/waterfall.jpg", 
+     "../img/drzewo.jpg", "../img/drzewo.jpg",
+      "../img/gory.jpg", "../img/gory.jpg"]
+const kontener = document.querySelector(".container")
+let pierwszaKarta = null
+let drugaKarta = null
+let zablokowane = false
+
+//<-------------------\/--------->
 
 function resize() {
   canvas.width = window.innerWidth;
@@ -37,7 +51,7 @@ function draw() {
 
     ctx.beginPath();
     ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
-    ctx.fillStyle = ` rgba(255,255,255,${alpha})`;//rgb(123, 255, 0)
+    ctx.fillStyle = ` rgb(123, 255, 0)`;//rgb(123, 255, 0)
     ctx.fill();
   });
 
@@ -46,7 +60,6 @@ function draw() {
 
 draw();
 
-//<--------------------------------------------GRA / LOGIKA------------------------------------?
 
 //logika gry
 let ruchy = 0;
@@ -80,6 +93,89 @@ const reset = () => {
   document.querySelector('.ruchy-liczba').textContent = ruchy;
   document.querySelector('.pary-liczba').textContent = `${pary}/8`;
   document.querySelector('.czas-liczba').textContent = '0:00';
-}
+
+    //LOGIKA
+
+    document.querySelector('.container').innerHTML =""
+    pierwszaKarta = null
+    drugaKarta = null
+    zablokowane = false
+   karty.sort(function() {
+    return Math.random() - 0.5
+  })
+karty.forEach(function(wartosc) {
+    generujKarty(wartosc)
+})
+} 
 
 przycisk.addEventListener('click', reset);
+//<--------------------------------------------GRA / LOGIKA------------------------------------?
+
+
+const generujKarty = (wartosc) => {
+  const karta = document.createElement("div")
+  const przod = document.createElement("div")
+  const tyl = document.createElement("div")
+  
+   karta.classList.add("karta")        
+   przod.classList.add("karta__przod")
+   tyl.classList.add("karta__tyl") 
+ 
+  const img = document.createElement("img")
+  img.src = wartosc
+  przod.appendChild(img)
+
+  karta.addEventListener("click" , function(){
+   if(zablokowane) return
+    if(karta === pierwszaKarta) return
+    if(karta.classList.contains("dopasowana")) return
+    if(pierwszaKarta === null ){
+      pierwszaKarta = karta
+        karta.classList.add("odkryta")
+      }else{
+      zablokowane = true
+      drugaKarta = karta 
+      karta.classList.add("odkryta")
+       ruchy += 1
+       document.querySelector('.ruchy-liczba').textContent = ruchy
+      if(pierwszaKarta.dataset.wartosc === drugaKarta.dataset.wartosc){
+        pierwszaKarta.classList.add("dopasowana")
+        drugaKarta.classList.add("dopasowana")
+        pary += 1
+       document.querySelector('.pary-liczba').textContent = `${pary}/6`
+       if(pary === 6){
+        clearInterval(timer)
+       }
+        pierwszaKarta = null
+        drugaKarta = null
+        zablokowane = false
+        
+      }else{
+        setTimeout(function(){
+          pierwszaKarta.classList.remove("odkryta")
+          drugaKarta.classList.remove("odkryta")
+          pierwszaKarta= null
+          drugaKarta = null
+          zablokowane = false
+        }, 1000)
+      }
+    }
+
+  })
+
+  karta.appendChild(przod)
+  karta.appendChild(tyl)
+  kontener.appendChild(karta)
+  karta.dataset.wartosc = wartosc
+
+}
+
+karty.sort(function() {
+    return Math.random() - 0.5
+})
+
+karty.forEach(function(wartosc){
+  
+  generujKarty(wartosc)
+  
+})
